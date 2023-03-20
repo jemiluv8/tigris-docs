af6c8a75cf8850822b8035d006720d7d21e9d6f0
Phil Leggetter <phil@leggetter.co.uk>
2023-03-20 13:56:37 +0000

    chore: remove player controls on mongodb video (#335)

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -44,7 +44,7 @@ export default function Homepage() {
               <iframe
                 width="560"
                 height="315"
-                src="https://www.youtube.com/embed/A8WDwXD4nvE"
+                src="https://www.youtube.com/embed/A8WDwXD4nvE?controls=0"
                 title="YouTube video player"
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
