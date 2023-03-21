c9052c019bc40b2ccfc0afeee386ac5b30469e08
Phil Leggetter <phil@leggetter.co.uk>
2023-03-21 10:55:48 +0000

    chore: MongoDB announcement bar (#339)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -105,11 +105,12 @@ const config = {
         },
       },
       announcementBar: {
-        id: "announcementBar-1", // increment on change
-        // content: `🦄 We've just launched Tigris MongoDB compatibility in beta. <a href="https://www.tigrisdata.com/blog/mongodb-compatibility-beta/">Read the announcement</a> 📣 or  <a href="https://www.tigrisdata.com/docs/concepts/mongodb-compatibility/">Check out the docs</a> 📖`,
-        content: `🚀 <a target="_blank" href="${tigrisConfig.signupUrl}" >Signup</a> for the Tigris beta and ⭐️ give us a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/tigrisdata/tigris">GitHub</a>`,
+        id: "announcementBar-2", // increment on change
+        content: `🦄 We've just launched Tigris MongoDB compatibility in beta. <a href="https://www.tigrisdata.com/blog/mongodb-compatibility-beta/">Read the announcement</a> 📣 or  <a href="https://www.tigrisdata.com/docs/concepts/mongodb-compatibility/">Check out the docs</a> 📖`,
+        // content: `🚀 <a target="_blank" href="${tigrisConfig.signupUrl}" >Signup</a> for the Tigris beta and ⭐️ give us a star on <a target="_blank" rel="noopener noreferrer" href="https://github.com/tigrisdata/tigris">GitHub</a>`,
         backgroundColor: "#5ecbad",
         textColor: "#262b31",
+        isCloseable: false,
       },
       navbar: {
         hideOnScroll: false,
