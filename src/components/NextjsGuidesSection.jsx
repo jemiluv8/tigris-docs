ce2e818da06e8b655165115f6c0384a37970cb4f
Ovais Tariq <ot@tigrisdata.com>
2023-04-07 09:59:11 -0700

    SEO related upadates (#363)

--- src/components/NextjsGuidesSection.jsx
+++ src/components/NextjsGuidesSection.jsx
@@ -10,9 +10,19 @@ export default function NextjsGuidesSection({ title, className }) {
     <div>
       <Section title="Tutorials" className={className}>
         <Card
-          title="Add Tigris to an existing Next.js Application"
+          title="Add Tigris to an existing Next.js app"
           description="This guide will help you add Tigris to an existing Next.js application"
           to="/guides/nextjs/add-existing-app/"
+        />
+        <Card
+          title="Next.js app with Tigris using create-next-app"
+          description="How to create a new Next.js project with Tigris using create-next-app"
+          to="https://www.tigrisdata.com/blog/create-next-app-with-tigris/"
+        />
+        <Card
+          title="Real-time full-text search in a Next.js app with Tigris"
+          description="Adding real-time full-text search to a Next.js app with Tigris"
+          to="https://www.tigrisdata.com/blog/adding-real-time-full-text-search-to-a-next.js-app-with-tigris/"
         />
       </Section>
 
